4f6740ddf0ac1b4f0b52b35aa999ef8cb40ea643
Salman Ahmad <naved.nice@gmail.com>
2023-03-26 23:54:59 +0530

    Removing unused import

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Session;
 
 class BookController extends Controller
 {
